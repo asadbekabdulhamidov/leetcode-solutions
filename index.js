bb3a467521dce1795a94de2646631e536c09a434
asadbekabdulhamidov <aabdulhamidov818@gmail.com>
2025-07-22 17:26:13 +0500

    leetcode problem one twoSum

---
+++ index.js
@@ -0,0 +1,18 @@
+const twoSum = function (nums, target) {
+  const map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i]; // 7
+    if (map.has(complement)) {
+      // 7 map ichida bormi ? false
+      return [map.get(complement), i];
+    }
+    map.set(nums[i], i); // mapga qoshadi numsni o-sini shudaynmi
+  }
+};
+
+let res = twoSum([2, 7, 11, 15], 9);
+// let res = twoSum([3,2,4], 6);
+// let res = twoSum([3,3], 6);
+
+//===============================================

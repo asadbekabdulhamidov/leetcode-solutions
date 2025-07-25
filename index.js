262947a30a92ad1f24a2420d80a9c3a6581d1a34
asadbekabdulhamidov <aabdulhamidov818@gmail.com>
2025-07-25 17:04:36 +0500

    problems

--- index.js
+++ index.js
@@ -1,30 +1,93 @@
-const twoSum = function (nums, target) {
-  const map = new Map();
+// const twoSum = function (nums, target) {
+//   const map = new Map();
 
-  for (let i = 0; i < nums.length; i++) {
-    const complement = target - nums[i]; // 7
-    if (map.has(complement)) {
-      // 7 map ichida bormi ? false
-      return [map.get(complement), i];
-    }
-    map.set(nums[i], i); // mapga qoshadi numsni o-sini shudaynmi
-  }
-};
+//   for (let i = 0; i < nums.length; i++) {
+//     const complement = target - nums[i]; // 7
+//     if (map.has(complement)) {
+//       // 7 map ichida bormi ? false
+//       return [map.get(complement), i];
+//     }
+//     map.set(nums[i], i); // mapga qoshadi numsni o-sini shudaynmi
+//   }
+// };
 
-let res = twoSum([2, 7, 11, 15], 9);
+// let res = twoSum([2, 7, 11, 15], 9);
 // let res = twoSum([3,2,4], 6);
 // let res = twoSum([3,3], 6);
 
 //===============================================
 
 // problem 2 isPolindrome
-function isPalindrome(num) {
-  let arrNum = num.toString().split('').reverse().join('');
-  if (Number(arrNum) === num) {
-    return true;
-  } else {
-    return false;
-  }
-}
+// function isPalindrome(num) {
+//   let arrNum = num.toString().split('').reverse().join('');
+//   if (Number(arrNum) === num) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
 
 // ==============================
+// 1-masala: Juft sonlar yig‘indisi
+// Vazifa:
+// Berilgan butun son n uchun 1 dan n gacha bo‘lgan faqat juft sonlar yig‘indisini qaytaruvchi funksiya yozing.
+// function sumEven(num) {
+//   let counter = 0;
+//   for (let i = 1; i <= num; i++) {
+//     if (i % 2 === 0) {
+//       counter += i;
+//     }
+//   }
+//   return counter;
+// }
+
+// let res = sumEven(10); // 2 + 4 + 6 + 8 + 10 = 30
+// console.log(res);
+
+//====================================================
+
+// 2-masala: Eng uzun so‘z
+// Vazifa:
+// Berilgan matndan eng uzun so‘zni toping. Faqat birinchi uchragan eng uzun so‘zni qaytaring.
+
+// function longestWord(str) {
+//   let longWord = '';
+//   let strArr = str.split(' ').forEach((word) => {
+//     if (longWord.length < word.length) {
+//       longWord = word;
+//     }
+//   });
+
+//   return longWord;
+// }
+
+// let res = longestWord('Men JavaScriptni yaxshi ko‘raman!'); // "JavaScriptni"
+
+// console.log(res);
+
+//==========================================================
+
+// 3-masala: FizzBuzz
+// Vazifa:
+// 1 dan n gacha bo‘lgan sonlarni quyidagi qoidalarga ko‘ra chop eting:
+// 3 ga bo‘linadigan son o‘rniga "Fizz"
+// 5 ga bo‘linadigan son o‘rniga "Buzz"
+// Ham 3, ham 5 ga bo‘linadigan son o‘rniga "FizzBuzz"
+// Aks holda o‘zini chiqaring
+
+// function fizzBuzz(n) {
+//   for (let i = 0; i <= n; i++) {
+//     if (i % 3 == 0 && i % 5 == 0) {
+//       console.log('FizzBuzz');
+//     } else if (i % 3 == 0) {
+//       console.log('Fizz');
+//     } else if (i % 5 == 0) {
+//       console.log('Buzz');
+//     } else {
+//       console.log(i);
+//     }
+//   }
+// }
+
+fizzBuzz(15);
+// 1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"

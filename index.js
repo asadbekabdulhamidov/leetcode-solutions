3faed29449af8cd71a01d2ca0e2dd7ad146c79d1
asadbekabdulhamidov <aabdulhamidov818@gmail.com>
2025-08-23 21:17:03 +0500

    anagram string

--- index.js
+++ index.js
@@ -2434,3 +2434,28 @@
 
 // console.log(flattenArray([[['a']], 'b', ['c', ['d', 'e']]]));
 // // ["a", "b", "c", "d", "e"]
+
+//=====================================================
+
+// Ikki ta string berilgan. Ular anagram ekanini tekshiring.
+
+// Anagram — bir xil harflardan tashkil topgan, faqat tartibi boshqacha bo‘lgan so‘z.
+// Masalan: "listen" va "silent" → anagram.
+
+function isAnagram(str1, str2) {
+  str1 = str1.toLowerCase().replace(/\s+/g, '');
+  str2 = str2.toLowerCase().replace(/\s+/g, '');
+
+  if (str1.length !== str2.length) return false;
+
+  return str1.split('').sort().join('') === str2.split('').sort().join('');
+}
+
+console.log(isAnagram('listen', 'silent'));
+// true
+
+console.log(isAnagram('hel lo', 'world '));
+// false
+
+console.log(isAnagram('rail safety', 'fairy tales'));
+// true

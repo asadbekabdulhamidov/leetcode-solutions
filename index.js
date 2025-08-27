bdc91547ae796de5c9bc5ca7f656f8bda8685180
asadbekabdulhamidov <aabdulhamidov818@gmail.com>
2025-08-27 14:41:28 +0500

    calculateCategoryTotals

--- index.js
+++ index.js
@@ -2728,3 +2728,37 @@
 // ];
 
 // console.log(groupByCategory(products));
+
+//====================================================================
+// Senga mahsulotlar ro‘yxati berilgan (id, name, price, stock, category).
+// 👉 Vazifa: har bir kategoriya bo‘yicha umumiy qiymatni hisoblash.
+// gar products bo‘sh bo‘lsa → {} qaytariladi.
+// Har bir kategoriya faqat umumiy summani oladi.
+// price va stock musbat bo‘ladi (manfiy emas)
+// // Umumiy qiymat degani: price * stock.
+// Natija obyekt bo‘lishi kerak.
+
+const products = [
+  { id: 1, name: 'Olma', price: 10000, stock: 5, category: 'meva' },
+  { id: 2, name: 'Banan', price: 12000, stock: 3, category: 'meva' },
+  { id: 3, name: 'Non', price: 4000, stock: 10, category: 'nonvoy' },
+  { id: 4, name: 'Lag‘mon', price: 25000, stock: 2, category: 'taom' },
+  { id: 5, name: 'Uzum', price: 15000, stock: 4, category: 'meva' },
+];
+
+function calculateCategoryTotals(products) {
+  if (!products.length) {
+    return {};
+  }
+
+  return products.reduce((acc, curVal) => {
+    if (curVal.category in acc) {
+      acc[curVal.category] += curVal.price * curVal.stock;
+    } else {
+      acc[curVal.category] = curVal.price * curVal.stock;
+    }
+    return acc;
+  }, {});
+}
+
+console.log(calculateCategoryTotals(products));
